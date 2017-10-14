9b2b93053c6dae6083b8f4b56521b0c973916b61
EiderDiaz <eiderdiazmoraila@hotmail.com>
2017-10-14 15:09:25 -0600

    salvo por que ya no hay bugs de mapas

--- app/src/main/java/com/example/eider/carpoolinguniversitario/MainActivity.java
+++ app/src/main/java/com/example/eider/carpoolinguniversitario/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.eider.carpoolinguniversitario;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
+import com.example.eider.carpoolinguniversitario.Fragmentos.FourFragemnt;
+import com.example.eider.carpoolinguniversitario.Fragmentos.OneFragment;
+import com.example.eider.carpoolinguniversitario.Fragmentos.ThreeFragment;
+import com.example.eider.carpoolinguniversitario.Fragmentos.TwoFragment;
 import com.facebook.AccessToken;
 import com.facebook.login.widget.LoginButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.ruta,
+            R.drawable.add,
+            R.drawable.search,
+            R.drawable.avatar
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +42,67 @@ public class MainActivity extends AppCompatActivity {
         if (AccessToken.getCurrentAccessToken() == null){
             goLoginScree();
         }
+       else{
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            viewPager = (ViewPager) findViewById(R.id.viewpager);
+            setupViewPager(viewPager);
+
+            tabLayout = (TabLayout) findViewById(R.id.tabs);
+            tabLayout.setupWithViewPager(viewPager);
+            setupTabIcons();
+        }
 
 
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "Viajes");
+        adapter.addFragment(new TwoFragment(), "Publicar");
+        adapter.addFragment(new ThreeFragment(), "Buscar");
+        adapter.addFragment(new FourFragemnt(), "Perfil");
+
+
+        viewPager.setAdapter(adapter);
+    }
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
     private void goLoginScree() {
---
+++ app/src/main/java/com/example/eider/carpoolinguniversitario/Modelos/DatosViaje.java
@@ -0,0 +1,8 @@
+package com.example.eider.carpoolinguniversitario.Modelos;
+
+/**
+ * Created by Eider on 14/10/2017.
+ */
+
+public class DatosViaje {
+}
